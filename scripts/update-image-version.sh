388e06741cb4538a5c2bb9378958ebcf4d7a23e3
omics42 <omics42@gmail.com>
2024-10-12 14:41:12 +0400

    chore(release): publish
    - project: server 0.0.20

--- scripts/update-image-version.sh
+++ scripts/update-image-version.sh
@@ -8,5 +8,6 @@ fi
 
 # Find and replace in all .yaml files in the current directory and subdirectories
 find . -type f -name "*.yml" | while read -r FILE; do
+  echo "Updating $FILE"
   sed -i "s|ghcr.io/proxy-gram/${PACKAGE_NAME}:.*|ghcr.io/proxy-gram/${PACKAGE_NAME}:${PACKAGE_VERSION}|g" "$FILE"
 done
