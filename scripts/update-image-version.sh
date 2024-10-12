c35bff9b224ded76baf5aea6c82df182d058237d
omics42 <omics42@gmail.com>
2024-10-12 14:06:00 +0400

    chore: add argocd and apps

---
+++ scripts/update-image-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if PACKAGE_NAME and PACKAGE_VERSION are set
+if [ -z "$PACKAGE_NAME" ] || [ -z "$PACKAGE_VERSION" ]; then
+  echo "Error: PACKAGE_NAME or PACKAGE_VERSION is not set."
+  exit 1
+fi
+
+# Find and replace in all .yaml files in the current directory and subdirectories
+find . -type f -name "*.yaml" | while read -r FILE; do
+  sed -i "s|ghcr.io/proxy-gram/${PACKAGE_NAME}:.*|ghcr.io/proxy-gram/${PACKAGE_NAME}:${PACKAGE_VERSION}|g" "$FILE"
+done
