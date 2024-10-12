ba2352d8ad7c29fbbd27fdea517e78a0a54a76a3
omics42 <omics42@gmail.com>
2024-10-13 00:19:28 +0400

    chore(release): publish
    - project: server 0.0.23

--- scripts/update-image-version.js
+++ scripts/update-image-version.js
@@ -6,21 +6,23 @@ const packageName = process.env.PACKAGE_NAME;
 const packageVersion = process.env.PACKAGE_VERSION;
 
 const oldImageName = RegExp(`ghcr.io/proxy-gram/${packageName}:.*`, 'g');
-glob
-  .sync('**/*.y?(a)ml', {
-    ignore: ['node_modules/**/*'],
-  })
+const files = glob.sync('**/*.y?(a)ml', {
+  ignore: ['node_modules/**/*'],
+});
+console.log(`files`, files);
+const filteredFiles = files
   .map((file) => [
     file,
     fs.readFileSync(file, { encoding: 'utf-8' }).toString(),
   ])
-  .filter(([filename, file]) => oldImageName.test(file))
-  .forEach(([filename, file]) => {
-    fs.writeFileSync(
-      filename,
-      file.replace(
-        oldImageName,
-        `ghcr.io/proxy-gram/${packageName}:${packageVersion}`
-      )
-    );
-  });
+  .filter(([filename, file]) => oldImageName.test(file));
+console.log('filteredFiles', filteredFiles);
+filteredFiles.forEach(([filename, file]) => {
+  fs.writeFileSync(
+    filename,
+    file.replace(
+      oldImageName,
+      `ghcr.io/proxy-gram/${packageName}:${packageVersion}`
+    )
+  );
+});
